0c40e8741d8f92d9926b48dc24548297baee1796
Sarode-Vaibhav1234 <sarodevaibhav20@gmail.com>
2023-01-11 22:15:42 +0530

    git add -A git commit -m "All exe files are deleted" git push

--- Unit II programs (Classes and Objects)/Train_class_prg_1.cpp
+++ Unit II programs (Classes and Objects)/Train_class_prg_1.cpp
@@ -12,50 +12,52 @@ Member Functions:
  - Display Data 
 Write a C++ program to test the train class.*/
 #include<iostream>
+#include<string.h>
 using namespace std;
-class train
+class Train
 {
-	private:
-		int no;
-		char name[10];
-		char source[20];
-		char destination[20];
-		int capacity;
-		char date[10];
-	public:
-		void initialize()
-		{
-			cout<<"\n Enter Train number:";
-			cin>>no;
-			cout<<"\n Enter Train Name:";
-			cin>>name;
-			cout<<"\n Enter Train source location:";
-			cin>>source;
-			cout<<"\n Enter Train destination location:";
-			cin>>destination;
-			cout<<"\n Enter Journey date of train:";
-			cin>>date;
-			cout<<"\n Enter capacity of train:";
-			cin>>capacity;
-		}
-		void display_train_info()
-		{
-			cout<<"\n ***************** Train Information *****************";
-			cout<<"\n Train no:"<<no;
-			cout<<"\n Train Name:"<<name;
-			cout<<"\n Train Source Location:"<<source;
-			cout<<"\n Train Destination Location:"<<destination;
-			cout<<"\n Journey Date:"<<date;
-			cout<<"\n Capacity of train:"<<capacity;
-		}
-}; 
+    int Train_Number; 
+    char Train_Name[20]; 
+    char Source[20]; 
+    char Destination[20]; 
+    char Journey_Date[10]; 
+    int Capacity; 
+    public:
+        Train()
+        {
+            Train_Number=0;
+            strcpy(Train_Name,"");
+            strcpy(Source,"");
+            strcpy(Destination,"");
+            strcpy(Journey_Date,"");
+            Capacity=0;
+        }
+        void get_data()
+        {
+            cout<<"\n Enter Train Number:";
+            cin>>Train_Number;
+            cout<<"\n Enter Train Name:";
+            cin>>Train_Name;
+            cout<<"\n Enter Train Source:";
+            cin>>Source;
+            cout<<"\n Enter Train Destination:";
+            cin>>Destination;
+            cout<<"\n Enter Train Journey Date:";
+            cin>>Journey_Date;
+        }
+        void disp_data()
+        {
+            cout<<"\n Train Number:"<<Train_Number;
+            cout<<"\n Train Name:"<<Train_Name;
+            cout<<"\n Train Source Location:"<<Source;
+            cout<<"\n Train Destination Location:"<<Destination;
+            cout<<"\n Train Journey Date:"<<Journey_Date;
+        }
+};
 int main()
 {
-	train t1,t2;
-	t1.initialize();
-	t2.initialize();
-	t1.display_train_info();
-	t2.display_train_info();
-	return 0;
-	
+    Train t1;
+    t1.get_data();
+    t1.disp_data();
+    return 0;
 }
